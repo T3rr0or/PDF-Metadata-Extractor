90756f023811ac2562443a3d24f181b16861bd18
Stefanvl_ <33265443+Stefanvl-Git@users.noreply.github.com>
2023-04-19 10:57:00 +0200

    v2

--- run.py
+++ run.py
@@ -2,13 +2,15 @@ import os
 from pathlib import Path
 
 mydir = Path("")
-for file in mydir.glob('*.pdf'):
-    command = "sha256sum"
-    command2 = "pdfx"
-    print(" ")
-    print(" ")
-    os.system(command + " " + "'" + file.name + "'")
-    print(" ")
-    os.system(command2 + " " + "'" + file.name + "'")
-
-    
+with open("output.txt", "w") as output_file:
+    for file in mydir.glob('*.pdf'):
+        command1 = "sha256sum"
+        command2 = "pdfx"
+        output_file.write(f"\n\n{file.name}:\n")
+        output_file.write("-" * 50 + "\n")
+        output_file.write(f"{command1}:\n")
+        output = os.popen(f"{command1} '{file.name}'").read()
+        output_file.write(output)
+        output_file.write(f"{command2}:\n")
+        output = os.popen(f"{command2} '{file.name}'").read()
+        output_file.write(output)
